1525f7a79f8974716f81e0f714048900a82aa105
malaotou <lei.ma@ricsource.com>
2021-12-01 08:50:54 +0800

    modify

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/xml"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "hello world"})
+	router.GET("/:name", func(c *gin.Context) {
+		name := c.Params.ByName("name")
+		c.XML(200, Person{FirstName: "Mohamed", LastName: "name" + name})
 	})
 	router.Run(":3000")
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
 }
